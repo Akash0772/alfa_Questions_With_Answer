a4ab8eac75408cb9a8791ff5b7f25fffdf00bd57
Akash0772 <akashchaurasia617@gmail.com>
2023-02-24 23:54:01 +0530

    Print PALINDROMIC Pattern with Numbers pattern

---
+++ A8 Pattern Advanced (Part - 2)/src/com/akash/P11.java
@@ -0,0 +1,28 @@
+package com.akash;
+// Print PALINDROMIC Pattern with Numbers pattern
+/*
+        1 
+      2 1 2 
+    3 2 1 2 3 
+  4 3 2 1 2 3 4 
+5 4 3 2 1 2 3 4 5 
+ */
+public class P11 {
+	public static void palindromic(int n) {
+		//outer loop
+		for(int i = 1; i <= n; i++) {
+			//inner loop
+			// space -> n - i;
+			for(int j = 1; j <= n - i; j++) {
+				 System.out.print("  ");
+			 }
+			       for(int j = 1; j <= 2 * i - 1 ; j++) {
+		    		   System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		palindromic(5);
+	}
+}
